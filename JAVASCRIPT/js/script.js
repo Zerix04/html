77dd1372b59dd1e354feef561525edc3aa51724d
Zerix04 <114378095+Zerix04@users.noreply.github.com>
2022-09-26 11:51:07 +0700

    Add files via upload

---
+++ JAVASCRIPT/js/script.js
@@ -0,0 +1,44 @@
+
+// var nama = "akbar";
+// document.write("Nama Saya : " + nama);
+
+// document.write("<br>");
+
+// var umur = 16
+// document.write("Umur      : " + umur);
+
+// alert("selamat datang di website");
+
+// YT
+
+// var ytb = confirm("apakah anda ingin melihat jawabannya?");
+
+// if (ytb) {
+//     window.location = "";
+// }
+
+// else {
+//     document.write("ok,sepertinya kamu sudah betah disini");
+// }
+
+// -prompt-
+
+// var nama = prompt("siapa nama kamu?", "");
+
+// document.write("Hello Nama kamu " + nama + "ya?")
+
+// var nilai_a = 10;
+// var nilai_b = 2;
+
+// var hasil = nilai_a + nilai_b;
+
+// document.write(hasil);
+
+
+// alas = 5;
+// tinggi = 8;
+
+// var luas = 0.5 * alas * tinggi;
+
+
+
